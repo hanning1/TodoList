dedf2bdd048524ba232ddd73fe469880b1f7f5f5
wembleypmo <wayne@waynes-iMac.local>
2020-02-17 14:04:36 +1100

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class About extends Component {
+	state = {};
+	render() {
+		return (
+			<React.Fragment>
+				<h1>About</h1>
+				<p>
+					This is the TodoList app v1.0.0. It is part of a React Crash
+					Course Tutorial and all the copyrights own to Traversy
+					Media. Video link:
+					https://www.youtube.com/watch?v=sBws8MSXN7A
+				</p>
+			</React.Fragment>
+		);
+	}
+}
+
+export default About;
